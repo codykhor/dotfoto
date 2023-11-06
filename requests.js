a1e267b724e4b99957d101ff982595aeea947b0e
Cody Khor <codykhor@gmail.com>
2023-11-06 12:02:09 +1000

    added sqs request to runner

--- requests.js
+++ requests.js
@@ -3,7 +3,7 @@ const FormData = require("form-data");
 const fs = require("fs");
 
 const localFilePath = "/Users/codykhor/Downloads/example_1.mov";
-const MAX = 5;
+const MAX = 3;
 
 const runRequests = async () => {
   for (let i = 0; i < MAX; i++) {
@@ -15,7 +15,7 @@ const runRequests = async () => {
       );
 
       const response = await axios.post(
-        "http://3.26.70.73:3000/upload",
+        "http://localhost:3000/upload",
         formData,
         {
           headers: {
@@ -40,9 +40,29 @@ const runRequests = async () => {
         if (uploadResponse.status === 200) {
           console.log("File uploaded successfully to S3!");
 
+          // Send the SQS message here
+          const sqsResponse = await axios.post(
+            "http://localhost:3000/send-sqs-message",
+            {
+              filename: filename,
+              message: "Your SQS message data here", // Add the message you want to send
+            },
+            {
+              headers: {
+                "Content-Type": "application/json",
+              },
+            }
+          );
+
+          if (sqsResponse.status === 200) {
+            console.log("SQS message sent successfully!");
+          } else {
+            console.log("Error sending SQS message.");
+          }
+
           // go to download page
           const downloadResponse = await axios.get(
-            `http://3.26.70.73:3000/download?name=${filename}`
+            `http://localhost:3000/download?name=${filename}`
           );
           if (downloadResponse.status === 200) {
             console.log("Download request successful");
